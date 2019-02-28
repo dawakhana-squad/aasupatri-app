ddd21f3cb7f753051f6c043cc2fe0017b0548076
mokshithpyla <mokshithpyla@mgmail.com>
2019-03-01 01:30:16 +0530

    Default Current Location with Marker

--- project1/App.js
+++ project1/App.js
@@ -1,95 +1,87 @@
-import React, {Component} from 'react';
-import { StyleSheet, View} from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import CurrentLocation from './components/CurrentLocation';
-//import Login from './src/pages/login';
-export default class App extends Component{
+import React, { Component } from 'react';
 
-  
-    constructor(props){
-        super(props);
-        this.state={
-          latitude:0,
-          longitude:0,
-          latlng:{'latitude':36.4220,'longitude':-125.0841},
-          long:0,
-          error:null,
-          markers:{latlng:{'latitude':36.4220,'longitude':-125.0841}},
-        };
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+
+import MapView, { Marker } from 'react-native-maps';
+
+const {width, height} = Dimensions.get('window')
+
+const SCREEN_HEIGHT = height
+const SCREEN_WIDTH = width
+const ASPECT_RATIO = SCREEN_HEIGHT / SCREEN_WIDTH
+const LATITUDE_DELTA = 0.0922
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      initialPosition: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      },
+    }
   }
 
-  getHospitalLocation=()=>{
-    this.setState({
-      lat:36.4220,
-      long:-125.0841,
-    });
-  }
-  componentDidMount(){
-    navigator.geolocation.getCurrentPosition(
-      position=>{
-        this.setState({
-          latitude:position.coords.latitude,
-          longitude:position.coords.longitude,
-          error:null
-        });
-      },error=>this.setState({error:error.message})
-    );
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var long = parseFloat(position.coords.longitude)
 
+      var initialRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
+
+      this.setState({initialPosition: initialRegion})
+    },
+    (error) => alert(JSON.stringify(error)),
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000});
   }
-      // fetch('https://whylikethis-b14f1.firebaseio.com/hospitals.json')
-    // .then(res=>res.json())
-    // .then(parsedRes => {
-    //   const placesArray=[];
-    //   for(const key in parsedRes){
-    //     placesArray.push({
-    //       latitude:parsedRes[key].latitude,
-    //       longitude:parsedRes[key].longitude,
-    //       id:key
-    //     });
-    //   }
+
+
+  renderScreen = () => {
+      return (
+        <View style={styles.container}>
+          <MapView
+            style={styles.map}
+            initialRegion={this.state.initialPosition}>
+            <Marker coordinate={this.state.initialPosition}/>
+            </MapView>
+        </View>
+      );
+  }
+
   render() {
-    // const userMarker=props.userPlaces.map(userPlace=>(<MapView.Marker coordinate={userPlace} key={userPlace.id}/>);
     return (
-      <View style={styles.container}>
-        <MapView
-            style={styles.map} 
-            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-            initialRegion={{ 
-            latitude: this.state.latitude,
-            longitude: this.state.longitude,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-            showsUserLocation: true,
-                      }}
-            
-          >
-               {this.state.markers.map(marker => (
-    <Marker
-              pinColor="blue"
-      coordinate={marker.latlng}
-      title={marker.title}
-      description={marker.description}
-    />
-  ))}
-          </MapView>
-          {/* <MapView.Marker {...this.getHospitalLocation} 
-          coordinate= {{'latitude':this.state.lat,'longitude':this.state.long}} pinColor='red'/> */}
-      </View>    
-          );
+      this.renderScreen()
+    );
   }
 }
-const styles = StyleSheet.create({
- container: {
-   ...StyleSheet.absoluteFillObject,
-   flex:1,
-  //  height: 400,
-  //  width: 400,
-   justifyContent: 'flex-end',
-   alignItems: 'center',
- },
- map: {
-   ...StyleSheet.absoluteFillObject,
-   flex:1,
 
- },
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
 });
